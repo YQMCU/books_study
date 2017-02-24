555b9ac49a68cf80188e3b8f2fb11b4704ebe16c
YQMCU <527896367@qq.com>
2017-02-25 00:24:36 +0800

    Modify:Modify codes directions

--- 02-web_scraping_with_Python/codes/ch02/bs4_find_demo.py
+++
@@ -1,8 +0,0 @@
-# -*- coding = utf-8 -*-
-from bs4 import BeautifulSoup
-broken_html = '<ul class="country"><li>Area<li>Population</ul>'
-# parse the HTML
-soup = BeautifulSoup(broken_html,"html.parse")
-ul = soup.find('ul',attrs={'class':'country'})
-ul.find('li') # returns just the first match
-ur.findall('li') # returns all matches
--- 02-web_scraping_with_Python/codes/ch02/lxml_css_demo.py
+++
@@ -1,9 +0,0 @@
-# -*- coding = utf-8 -*-
-from url_download import download
-import lxml.html
-url = "http://example.webscraping.com/places/view/United-Kingdom-239"
-html = download(url)
-tree = lxml.html.fromstring(html) # parse the HTML
-td = tree.cssselect('tr#places_area__row > td.w2p_fw')[0]
-area = td.text_content()
-print(area)
