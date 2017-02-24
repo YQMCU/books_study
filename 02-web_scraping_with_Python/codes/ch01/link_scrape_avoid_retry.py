ecd487e40ae748e7529a289daef5c72b7a025aa0
YQMCU <527896367@qq.com>
2017-02-25 01:13:50 +0800

    Add:links scraping generate absolute direction

---
+++ 02-web_scraping_with_Python/codes/ch01/link_scrape_avoid_retry.py
@@ -0,0 +1,31 @@
+# -*- coding : utf-8 -*-
+
+import url_download
+import re
+
+def link_crawler(seed_url,link_regex):
+    """Crawl from the given seed URL following links matched by link_regex
+    """
+    crawl_queue = [seed_url]
+    # keep track which URL's have seen before
+    while crawl_queue:
+        url = crawl_queue.pop()
+        html = download(url)
+        # filter for links matching our regular expression
+        for link in get_links(html):
+            # check if link matches expected regex
+        	if re.match(link_regex,link):
+                # form absolute link
+                link = urlparse.urljoin(seed_url,link)
+                # check if have already seen this link
+                if link not in seen:
+                    seen.add(link)
+                    crawl_queue.append(link)
+
+def get_links(html):
+	"""Return a list of links from html
+	"""
+	# a regular expression to extract all links from the webpage
+	webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']',re.IGNORECASE)
+	# list of all links from the webpage
+	return webpage_regex.findall(html)
