11eccb6273d1c5e82b53396b5d26e9033c377c48
YQMCU <527896367@qq.com>
2017-02-24 23:49:31 +0800

    Modify:judge regex result is null

--- scripts/markdown_parse.js
+++ scripts/markdown_parse.js
@@ -5,7 +5,9 @@
 	document.body.removeChild(md_body);
 	var re = /书名：(.*?)\s/
 	var title = re.exec(md_body_content);
-	document.getElementsByTagName("title")[0].innerHTML = title[1];
+	if(title){
+		document.getElementsByTagName("title")[0].innerHTML = title[1];		
+	}
 
 	var xmp = document.createElement("xmp");
 	xmp.setAttribute("theme","united");
